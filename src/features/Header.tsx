36108815997e7dc4211dc492a5a08e267b1238a4
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2022-05-16 09:36:58 +0900

    Super WIP

--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -1,4 +1,13 @@
 import { VoidFunctionComponent } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-regular-svg-icons";
+import {
+  faArrowRotateLeft,
+  faArrowRotateRight,
+  faTrashCan,
+  faFillDrip,
+  faPen,
+} from "@fortawesome/free-solid-svg-icons";
 
 type HeaderProps = {};
 
@@ -6,18 +15,14 @@ export const Header: VoidFunctionComponent<HeaderProps> = (props) => {
   return (
     <nav
       className="w-screen flex justify-between shrink-0 bg-slate-100 px-4 items-center border-b"
-      style={{ height: "3rem" }}
+      style={{ height: "2rem" }}
     >
-      <div className="flex items-center gap-4">
-        <figure className="rounded-md">
-          <img src="https://i.pravatar.cc/32" className="rounded-md" />
-        </figure>
-        <p>Draw Editor</p>
-      </div>
-      <div>
-        <div className="bg-lime-500 hover:bg-lime-700 text-white py-1 px-4 rounded">
-          Submit
-        </div>
+      <div className="flex gap-2">
+        <FontAwesomeIcon icon={faArrowRotateLeft} />
+        <FontAwesomeIcon icon={faArrowRotateRight} />
+        <FontAwesomeIcon icon={faTrashCan} />
+        <FontAwesomeIcon icon={faFillDrip} />
+        <FontAwesomeIcon icon={faPen} />
       </div>
     </nav>
   );
